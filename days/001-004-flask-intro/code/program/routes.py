2cd156dd38b54662aa6ff1ff680ff77c7df563dc
Cristóbal Contreras Rubio <crisconru@gmail.com>
2020-03-02 17:34:39 +0100

    days001-004

--- days/001-004-flask-intro/code/program/routes.py
+++ days/001-004-flask-intro/code/program/routes.py
@@ -1,9 +1,11 @@
+from datetime import datetime
 from flask import render_template
 from program import app
 
 @app.route('/')
 @app.route('/index')
 def index():
+    timenow = datetime.now()
     return render_template('index.html', title='Template Demo', time=timenow)
 
 @app.route('/100Days')
